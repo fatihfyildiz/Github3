5b64229489670f4983e25f153ba3b6df0a5e3ee1
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2022-10-01 09:08:59 +0200

    Revert "C03"
    This reverts commit 0c6d7d35e08cf762a7b0aac679f52e0f94146afe.

--- src/C02.java
+++ src/C02.java
@@ -1,7 +1,2 @@
 public class C02 {
-
-    public static void main(String[] args) {
-
-        System.out.println("C02");
-    }
 }
--- src/C03.java
+++
@@ -1,7 +0,0 @@
-public class C03 {
-
-    public static void main(String[] args) {
-
-        System.out.println("C03");
-    }
-}
